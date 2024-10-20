521791d439eece67a1cfd52b94c1dd45d96bac17
7DARKNESS7 <jorika911@gmail.com>
2024-10-20 20:37:21 +0400

    added api functions

---
+++ js/cart.js
@@ -0,0 +1,152 @@
+let productCards = document.getElementById("productCards");
+let categories = document.getElementById("categories");
+let cartProduct = document.getElementById("cartProd");
+let total = document.getElementById("cartTotal");
+let generalTotal = document.getElementById("generalTotal");
+
+async function getCart() {
+  let cartList = await fetch(
+    "https://restaurant.stepprojects.ge/api/Baskets/GetAll"
+  );
+  return cartList.json();
+}
+
+getCart().then((data) => {
+  showBasket(data);
+});
+
+function showBasket(list) {
+  let total = 0;
+
+  cartProduct.innerHTML = "";
+
+  list.forEach((item) => {
+    let fullPrice = item.quantity * item.price;
+
+    if (item.product.quantity >= 1) {
+      item.quantity++;
+    }
+
+    cartProduct.innerHTML += `
+      <div class="cart-item" id="item-${item.product.id}">
+        <div class="name-image">
+          <div class="deleteIcon">
+            <i class="fi fi-br-cross-small" data-id="${item.product.id}"></i>
+            <i class="fi fi-sc-pencil" data-id="${item.product.id}"></i>
+          </div>
+          <div class="imgName">
+            <img src="${item.product.image}" alt="">
+            <span>${item.product.name}</span>
+          </div>
+        </div>
+        <div class="qty">
+          <div>
+            <i class="fi fi-ss-add qtyBtn" onclick="quantityIncrease(${item.price}, ${item.quantity}, ${item.product.id})"></i>
+            <span>${item.quantity}</span>
+            <i class="fi fi-ss-minus-circle qtyBtn" onclick="quantityDecrease(${item.price}, ${item.quantity}, ${item.product.id})"></i>
+          </div>
+          <span>$ ${item.price}</span>
+          <span class="item-total">$ ${fullPrice}</span>
+        </div>
+      </div>
+    `;
+
+    total += fullPrice;
+  });
+
+  generalTotal.innerText = `Total: $${total.toFixed(2)}`;
+
+  document.querySelectorAll(".fi-br-cross-small").forEach((icon) => {
+    icon.addEventListener("click", function () {
+      const productId = this.getAttribute("data-id");
+      deleteProductFromCart(productId);
+    });
+  });
+}
+
+function quantityIncrease(cardPrice, cardQuantity, cartID) {
+  cardQuantity++;
+  let forUpdate = {
+    quantity: cardQuantity,
+    price: cardPrice,
+    productId: cartID,
+  };
+
+  fetch("https://restaurant.stepprojects.ge/api/Baskets/UpdateBasket", {
+    method: "PUT",
+    headers: {
+      accept: "*/*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(forUpdate),
+  }).then((res) =>
+    res.text().then((data) => {
+      getCart().then((cartItems) => showBasket(cartItems));
+    })
+  );
+}
+
+function quantityDecrease(cardPrice, cardQuantity, cartID) {
+  if (cardQuantity > 1) {
+    cardQuantity--;
+    let forUpdate = {
+      quantity: cardQuantity,
+      price: cardPrice,
+      productId: cartID,
+    };
+
+    fetch("https://restaurant.stepprojects.ge/api/Baskets/UpdateBasket", {
+      method: "PUT",
+      headers: {
+        accept: "*/*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(forUpdate),
+    }).then((res) =>
+      res.text().then((data) => {
+        getCart().then((cartItems) => showBasket(cartItems));
+      })
+    );
+  } else {
+    deleteProductFromCart(cartID);
+  }
+}
+
+function deleteProductFromCart(productId) {
+  fetch(
+    `https://restaurant.stepprojects.ge/api/Baskets/DeleteProduct/${productId}`,
+    {
+      method: "DELETE",
+    }
+  )
+    .then((response) => {
+      if (response) {
+        document.getElementById(`item-${productId}`).remove();
+        recalculateTotal();
+      } else {
+        console.error("Failed to delete the product.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function recalculateTotal() {
+  let newTotal = 0;
+
+  document.querySelectorAll(".item-total").forEach((item) => {
+    newTotal += parseFloat(item.innerText.replace("$", ""));
+  });
+
+  generalTotal.innerText = `Total: $${newTotal.toFixed(2)}`;
+}
+
+window.onscroll = function () {
+  let navbar = document.getElementById("navbarScroll");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navbar.classList.add("navScrolled");
+  } else {
+    navbar.classList.remove("navScrolled");
+  }
+};
---
+++ js/home.js
@@ -0,0 +1,194 @@
+let productCards = document.getElementById("productCards");
+let categories = document.getElementById("categories");
+let cartProduct = document.getElementById("cartProd");
+let total = document.getElementById("cartTotal");
+let generalTotal = document.getElementById("generalTotal");
+let addToCartBtn = document.getElementById("addToCartBtn");
+let nuts = document.getElementById("nuts");
+let vegan = document.getElementById("vegan");
+let spicinessRange = document.getElementById("spicinessRange");
+let spicinessValue = document.getElementById("spicinessValue");
+let filterReset = document.getElementById("filterReset");
+let filterApply = document.getElementById("filterApply");
+let filterResult = document.getElementById("filterResult");
+let errorBox = document.getElementById("errorBox");
+
+async function getProducts() {
+  let productList = await fetch(
+    "https://restaurant.stepprojects.ge/api/Products/GetAll"
+  );
+  return productList.json();
+}
+getProducts().then((data) => {
+  console.log(data);
+  showCards(data);
+});
+
+function showCards(items) {
+  productCards.innerHTML = "";
+
+  items.forEach((item) => {
+    productCards.innerHTML += `
+        <div class="pCard">
+          <img src="${item.image}" alt="">
+          <div class="cardInfo">
+            <h3>${item.name}</h3>
+            <p>Spiciness: ${item.spiciness}</p>
+            <div class="addIngr">
+              <p>${
+                item.nuts
+                  ? `<i class="fi fi-rs-circle"></i>`
+                  : `<i class="fi fi-rs-check-circle"></i>`
+              } nuts</p>
+              <p>${
+                item.vegeterian
+                  ? `<i class="fi fi-rs-check-circle"></i>`
+                  : `<i class="fi fi-rs-circle"></i>`
+              } vegetarian</p>
+            </div>
+            <div class="bottomInfo">
+              <p>$ ${item.price}</p>
+              <button onclick="addToCart(${item.price},${
+      item.id
+    })">Add to cart</button>
+            </div>
+          </div>
+        </div>
+      `;
+  });
+}
+
+fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    showCategories(data);
+  });
+
+function showAllProducts() {
+  fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
+    .then((response) => response.json())
+    .then((data) => {
+      showCards(data);
+    });
+}
+
+// Categories
+function showCategories(list) {
+  categories.innerHTML = '<span onclick="showAllProducts()">All</span>';
+
+  list.forEach((item) => {
+    categories.innerHTML += `
+      <span onclick="showCardsByCategory(${item.id})">${item.name}</span>
+    `;
+  });
+}
+
+function showCardsByCategory(id) {
+  fetch(`https://restaurant.stepprojects.ge/api/Categories/GetCategory/${id}`)
+    .then((response) => response.json())
+    .then((finalData) => {
+      let productList = finalData.products;
+      showCards(productList);
+    });
+}
+
+// Cart Add
+let cartItems = [];
+
+function addToCart(price, id) {
+  let existingItem = cartItems.find((item) => item.productId === id);
+
+  if (existingItem) {
+    existingItem.quantity++;
+
+    fetch("https://restaurant.stepprojects.ge/api/Baskets/UpdateBasket", {
+      method: "PUT",
+      headers: {
+        accept: "text/plain",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        quantity: existingItem.quantity,
+        price: price,
+        productId: id,
+      }),
+    })
+      .then(() => showBasket(cartItems))
+      .catch((error) => console.error("Error updating cart:", error));
+  } else {
+    let newItem = { productId: id, quantity: 1, price: price };
+    cartItems.push(newItem);
+
+    fetch("https://restaurant.stepprojects.ge/api/Baskets/AddToBasket", {
+      method: "POST",
+      headers: {
+        accept: "text/plain",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then(() => showBasket(cartItems))
+      .catch((error) => console.error("Error adding to cart:", error));
+  }
+}
+
+// Filter
+let spiciness = "";
+
+filterApply.addEventListener("click", function (e) {
+  e.preventDefault();
+  spicinessRange.value > "-1"
+    ? (spiciness = spicinessRange.value)
+    : (spiciness = "");
+  nuts.checked ? (nuts.value = true) : "";
+  vegan.checked ? (vegan.value = true) : "";
+
+  fetch(
+    `https://restaurant.stepprojects.ge/api/Products/GetFiltered?vegeterian=${vegan.value}&nuts=${nuts.value}&spiciness=${spiciness}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      if (Array.isArray(data)) {
+        if (data.length > 0) {
+          showCards(data);
+        } else {
+          errorBox.style.opacity = 1;
+          errorBox.style.transform = "translateY(50px)";
+
+          setTimeout(() => {
+            errorBox.style.transform = "translateY(-100px)";
+          }, 1500);
+        }
+      }
+      filterResult.innerText = "No Product Found!";
+    });
+});
+
+filterReset.addEventListener("click", function () {
+  fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
+    .then((response) => response.json())
+    .then((data) => {
+      showCards(data);
+    });
+  spicinessRange.value = "-1";
+  spicinessValue.innerText =
+    spicinessRange > -1 ? spicinessRange : "Not Chosen";
+  nuts.checked ? (nuts.checked = false) : "";
+  vegan.checked ? (vegan.checked = false) : "";
+});
+
+spicinessRange.addEventListener("input", function () {
+  let spicinessRangeVal = spicinessRange.value;
+  spicinessValue.innerText =
+    spicinessRangeVal > -1 ? spicinessRangeVal : "Not Chosen";
+});
+
+window.onscroll = function () {
+  let navbar = document.getElementById("navbarScroll");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navbar.classList.add("navScrolled");
+  } else {
+    navbar.classList.remove("navScrolled");
+  }
+};
